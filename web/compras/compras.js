d1af5e914fc6e029389ed4a19c37b4329878ed66
Jj <Jj@Juanjo>
2014-04-10 02:37:21 +0200

    Error solucionado, todos los botones funcionan

--- web/compras/compras.js
+++ web/compras/compras.js
@@ -1,5 +1,5 @@
 app.config(function($routeProvider) {
-    $routeProvider.when = ('/compras/compras',
+    $routeProvider.when('/compras/compras',
             {
                 templateUrl: 'compras/compras.html',
                 controller: "ComprasController"
